29f181439c61fa0fe73cadc3d10b8071add23852
mitutee <volman1285@gmail.com>
2017-04-12 08:10:07 +0300

    pull succsessfully

--- bubbleman/NodeBubble.js
+++ bubbleman/NodeBubble.js
@@ -11,19 +11,21 @@ class NodeBubble{
     constructor(gitUri,bubbleName){
         this.GitUri = gitUri;
         this.BubbleName = bubbleName;
-        // setting up this bubble
+
+        // defining up pathes
         this._cageName = `_bubble0${this.BubbleName}`;        
         this._baseCage = path.join(__dirname,"_bubbles");
         this._cage = path.join(this._baseCage,this._cageName);
         this._src = path.join(this._cage,"_src");
-        
-        this._inflateBubble();
+ 
+        // setting up this bubble        
+        this.package_json =  this._inflateBubble();
     }
 
     _inflateBubble(){
         this._ensureCageExists();
-        this._loadSourceCode();
-        
+        this._gitClone();
+        return this._get_package_json();        
     };
 
     _ensureCageExists(){
@@ -40,9 +42,41 @@ class NodeBubble{
         
     };
 
-    _loadSourceCode(){
-        exec(`git clone ${this.GitUri} ${this._src}`, __gitCloneCallback);
+    _gitClone(){
+        let command = `git clone ${this.GitUri} ${this._src}`;
+        let callback = this._gitCloneCallback.bind(this);
+        exec(command, callback);
     };
+    _gitCloneCallback(error, stdout, stderr){
+        if (error) {
+        console.error(`exec error: ${error}`);
+        console.error('AHTUNG!!1');
+            this._gitPull();
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    };
+    _gitPull(){
+        let command = `git --work-tree=${this._src} --git-dir=${this._src}/.git pull origin master`;
+        var callback = this._gitPullCallback.bind(this)
+        exec(command, callback);
+    }
+
+    _gitPullCallback(error, stdout, stderr){
+        if (error) {
+        console.error(`exec error: ${error}`);
+        console.error('AHTUNG!!1');
+            this._
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    };
+    _get_package_json(){
+        let p = path.join(this._src, 'package.json');
+        return JSON.parse(fs.readFileSync(p,'utf8'));        
+    }
 
     Deploy(){
         exec('dir', function(err, stdout, stderr){
@@ -52,14 +86,6 @@ class NodeBubble{
 }
 
 
-function __gitCloneCallback(error, stdout, stderr){
-  if (error) {
-  console.error(`exec error: ${error}`);
-  console.error('AHTUNG!!1');
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-}
+
 
 let bb = new NodeBubble('https://github.com/mitutee/node_Panel.git', 'test');
