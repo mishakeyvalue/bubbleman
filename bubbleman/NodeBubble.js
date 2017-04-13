67cbc9ac5ff84aad9c52471f878c5da46bd13d78
mitutee <volman1285@gmail.com>
2017-04-13 06:13:22 +0300

    Sequential setup Saving obj in _obj.json

--- bubbleman/NodeBubble.js
+++ bubbleman/NodeBubble.js
@@ -4,7 +4,8 @@ const fs        = require('fs'),
       path      = require('path'),
       _PLATFORM = require('os').platform(),
       spawn     = require('child_process').spawn,
-      exec      = require('child_process').exec
+      exec      = require('child_process').exec,
+      events    = require('events')
 
 
 class NodeBubble{
@@ -18,15 +19,33 @@ class NodeBubble{
         this._cage = path.join(this._baseCage,this._cageName);
         this._src = path.join(this._cage,"_src");
  
-        // setting up this bubble        
+        // setting up this bubble
+
+        this._deploymentEvents = new events.EventEmitter();
+        // Fires up when source code cloned / pulled
+        this._deploymentEvents.on('sourced', this.sourcedHandler.bind(this));
+        // Fires up when npm dependencies restored
+        this._deploymentEvents.on('restored', this.restoredHandler.bind(this));
+
         this._inflateBubble();
     }
+
+    // --- event handlers ---
+    sourcedHandler(){
+        // Job after downloading source code
+        this.package_json =  this._get_package_json();
+        this._stringifyBubble();
+        this._restore();                        
+    };
+    restoredHandler(){
+
+    };
+    // ---   ---
+// ************    PREPARING BUBBLE    ************
 
     _inflateBubble(){
         this._ensureCageExists();
         this._gitClone();
-        this.package_json =  this._get_package_json();        
-        this._restore();
     };
 
     _ensureCageExists(){
@@ -42,24 +61,8 @@ class NodeBubble{
         };
         
     };
-    _restore(){
-        for(let dependency in this.package_json.dependencies){
-            let command = `npm install ${dependency} ${this._src}`;
-            let callback = this._restoreCallback.bind(this);
-            exec(command, callback);
-        }
-      //  exec(command, callback)
-    };
-    _restoreCallback(error, stdout, stderr){
-        if (error) {
-           // console.error(`******************************exec error: ${error}`);
-            return;
-        }
-        console.log("RESOTRE CALL--------------------------------------------------");
-        
-      //  console.log(`stdout: ${stdout}`);
-       // console.log(`stderr: ${stderr}`);
-    }
+
+// --- getting source code ---
     _gitClone(){
         let command = `git clone ${this.GitUri} ${this._src}`;
         let callback = this._gitCloneCallback.bind(this);
@@ -75,10 +78,12 @@ class NodeBubble{
         console.log("GIT CLONE CALL--------------------------------------------------");
         //console.log(`stdout: ${stdout}`);
         //console.log(`stderr: ${stderr}`);
+        this._deploymentEvents.emit('sourced');        
     };
+
     _gitPull(){
         let command = `git --work-tree=${this._src} --git-dir=${this._src}/.git pull origin master`;
-        var callback = this._gitPullCallback.bind(this)
+        var callback = this._gitPullCallback.bind(this);
         exec(command, callback);
     }
 
@@ -89,15 +94,44 @@ class NodeBubble{
             return;
         }
         console.log("GIT PULL CALL--------------------------------------------------");
-        
        // console.log(`stdout: ${stdout}`);
        // console.log(`stderr: ${stderr}`);
+       this._deploymentEvents.emit('sourced');
+       
     };
+// ---  ---
+
+// --- restoring npm dependencies ---
+    _restore(){
+        for(let dependency in this.package_json.dependencies){
+            console.log("Restoring dependency: " + dependency)            
+            let command = `npm install ${dependency}@${this.package_json.dependencies[dependency]} ${this._src}`;
+            let callback = this._restoreCallback.bind(this);
+            exec(command, callback);
+        }
+      //  exec(command, callback)
+    };
+    _restoreCallback(error, stdout, stderr){
+        if (error) {
+           console.error(`******************************exec error: ${error}`);
+            return;
+        }
+        console.log("RESOTRE CALL--------------------------------------------------");
+        
+        //console.log(`stdout: ${stdout}`);
+        //console.log(`stderr: ${stderr}`);
+    }
+// ---   ---
+
     _get_package_json(){
         let p = path.join(this._src, 'package.json');
         return JSON.parse(fs.readFileSync(p,'utf8'));        
     }
-
+    _stringifyBubble(){
+        let p = path.join(this._cage, "_obj.json")
+        fs.writeFileSync(p, JSON.stringify(this));
+    }
+// ************    ************
     Deploy(){
         let pathToMain = path.join(this._src, this.package_json.main)
         let command = `pm2 start ${pathToMain} --watch`;
@@ -125,4 +159,5 @@ class NodeBubble{
 
 
 let bb = new NodeBubble('https://github.com/mitutee/node_Panel.git', 'test');
-bb.Deploy();
+
+console.log(JSON.stringify(bb))
