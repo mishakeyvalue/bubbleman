3d02698762c4d45365c1171c6e87832e8aee51af
mitutee <volman1285@gmail.com>
2017-04-21 16:14:08 +0300

    changes

--- dashboard/app.js
+++ dashboard/app.js
@@ -17,6 +17,3 @@ bubbleController(app);
 //---  ---
 app.listen(1337);
 console.log("Server running at http://127.0.0.1:1337/");
-
-
-console.log("Web-terminal accessible at http://127.0.0.1:1337/terminal");
