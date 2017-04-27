e5689b5da1618e3df0c36ddd756fb5d2373431f1
mitutee <fen1285@mail.ru>
2017-04-27 08:50:08 +0300

    Reloading encompassed into function

--- dashboard/controllers/apiController.js
+++ dashboard/controllers/apiController.js
@@ -4,6 +4,19 @@ module.exports = function(app){
     app.post('/api/tootee', function(req, res){       
 //        console.log(req)
         res.end('200');
+	tootee.reload();
+    });
+   app.post('/api/tootee', function(req, res){       
+	console.log(req);
+        res.end('200');
+	tootee.reload();
+    })
+};
+
+let tootee = {
+    _src : "/usr/HangBotReborn/HangBotReborn",
+    production_cage : "/var/myw/",
+    reload : function() {
         execSync("systemctl daemon-reload");
         execSync("systemctl stop tootee");
         let command = `git --work-tree=${tootee._src}/../ --git-dir=${tootee._src}/../.git pull origin master`;
@@ -13,10 +26,5 @@ module.exports = function(app){
 	console.log(`dotnet publish -o ${tootee.production_cage}`)
         execSync(`dotnet publish ${tootee._src}/HangBotReborn.csproj -o ${tootee.production_cage}`)
         execSync("systemctl start tootee");
-    })
-};
-
-let tootee = {
-    _src : "/usr/HangBotReborn/HangBotReborn",
-    production_cage : "/var/myw/"
+    }
 };
